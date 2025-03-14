2c8b46948aeb25e3c5a952f2dcc7b12ea4172471
craston <nievescrasto18@gmail.com>
2025-03-14 19:37:33 +0000

    feat - Add CLI support for SVM, neural network, and Isolation Forest training; include random seed initialization

--- cli.py
+++ cli.py
@@ -1,10 +1,18 @@
 from typer import Typer
 
 from preprocess import dataset
+from trainer import train_svm
+from trainer import train_nn
+from trainer import train_if
+from utils import seed_everything
 
 app = Typer()
 
 app.add_typer(dataset.app, name="ds", help="Split data")
+app.add_typer(train_svm.app, name="svm", help="Train SVM model")
+app.add_typer(train_nn.app, name="nn", help="Train neural network model")
+app.add_typer(train_if.app, name="if", help="Train Isolation Forest model")
 
 if __name__ == "__main__":
+    seed_everything(42)
     app()
